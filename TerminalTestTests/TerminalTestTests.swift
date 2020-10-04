fa90d34e42acb6b1665656d68762d8d3f5a7994e
Karthik <karthik.ms@zohocorp.com>
2020-10-04 13:07:00 +0530

    Added files

---
+++ TerminalTestTests/TerminalTestTests.swift
@@ -0,0 +1,31 @@
+//
+//  TerminalTestTests.swift
+//  TerminalTestTests
+//
+//  Created by Karthik on 03/10/20.
+//
+
+import XCTest
+
+class TerminalTestTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        XCTAssertEqual(1, 2)
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
