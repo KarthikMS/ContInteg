1f1867491bb66a14c458f8b03c40eff9d2a325b4
Karthik <karthik.ms@zohocorp.com>
2020-10-08 11:40:12 +0530

    macOS added

--- TerminalTest/ViewController.swift
+++ TerminalTest/ViewController.swift
@@ -11,7 +11,19 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+     
+        #if targetEnvironment(macCatalyst)
+        print("Running on mac")
+        #else
+        switch traitCollection.userInterfaceIdiom {
+        case .phone:
+            print("Running on iPhone")
+        case .pad:
+            print("Running on iPad")
+        default:
+            print("Running on \(traitCollection.userInterfaceIdiom)")
+        }
+        #endif
     }
 
 
